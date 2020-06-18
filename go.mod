559e83f546ea5454e0f4dc3f2c0a1cde9c3fd9bf
vnotes <vnotes@protonmail.com>
2020-06-18 15:28:58 +0800

    negroni

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/gorilla/mux v1.7.5-0.20200517040254-948bec34b516
 	github.com/prometheus/client_golang v1.6.0
 	github.com/stretchr/testify v1.6.2-0.20200613113352-b8f7d52a4a7c
+	github.com/urfave/negroni v1.0.1-0.20200608235619-7de0dfc1ff79
 	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9
 )
---
+++ httputil/negroni.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+)
+
+func APIHttpHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("calling")
+	_, _ = w.Write([]byte("I am here."))
+}
+
+func apiMiddleware(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	fmt.Println("here start")
+	next(rw, r)
+	fmt.Println("here end")
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", APIHttpHandler)
+	n := negroni.New()
+	// 中间件的使用必须在 use handler 之前
+	n.Use(negroni.HandlerFunc(apiMiddleware))
+	n.UseHandler(router)
+	_ = http.ListenAndServe(":3001", n)
+}
